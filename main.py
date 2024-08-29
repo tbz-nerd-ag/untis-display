60faadb209d185f45c2b6799bf4942855cbb3780
ingressy <j.czinzoll@ingressy.de>
2024-08-29 10:03:22 +0200

    a good start i guess

---
+++ main.py
@@ -0,0 +1,50 @@
+import datetime, os, webuntis.objects, random, getpass
+
+from dotenv import load_dotenv
+from prettytable import PrettyTable
+
+load_dotenv()
+SRV = os.getenv('SRV')
+USR = os.getenv('USR')
+PWD = os.getenv('PWD')
+SHO = os.getenv('SHO')
+USRA = os.getenv('USRA')
+STS = os.getenv('STS')
+
+s = webuntis.Session(
+        server=SRV,
+        username=USR,
+        password=PWD,
+        school=SHO,
+        useragent=USRA
+    )
+
+s.login()
+
+print("TBZ Mitte DoorSign Server v0.1\nby ingressy\n")
+choose = input("From which room would you like to have the timetable? ")
+chtime = input("What time is it? ")
+
+start = datetime.datetime.now()
+end = start + datetime.timedelta(days=4)
+
+rooms = s.rooms().filter(name=choose)
+
+tt = s.timetable(room=rooms[0], start=start, end=end)
+tt = sorted(tt, key=lambda x: x.start)
+#print(tt)
+
+time_format_end = "%H%M"
+time_format_start = "%Y-%m-%d %a " + time_format_end
+
+for po in tt:
+    s = po.start.strftime(time_format_start)
+    e = po.end.strftime(time_format_end)
+    k = " ".join([k.name for k in po.klassen])
+    t = " ".join([t.name for t in po.teachers])
+    r = " ".join([r.name for r in po.rooms])
+    sub = " ".join([r.name for r in po.subjects])
+    c = "(" + po.code + ")" if po.code is not None else ""
+
+    if chtime < e:
+        print(s + "-" + e, k, sub, t, r, c)
